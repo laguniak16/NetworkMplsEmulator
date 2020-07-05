7e6244bf73a128c8631149b7fc7d8493413c139e
Mateusz Łaguna <mateusz_laguna@o2.pl>
2020-07-05 11:03:42 +0200

    2

--- exe/ggMalyPi0otera.bat
+++
@@ -1,4 +0,0 @@
-start .\WindowsFormsAppCN.exe "..\\Config\\host1.xml"
-timeout 1
-start .\WindowsFormsAppNN.exe "..\\Config\\config1.xml"
-timeout 1
--- exe/test.bat
+++
@@ -1,5 +0,0 @@
-start .\Manager.exe "..\\Config\\managerConfig.xml"
-timeout 1
-start .\WindowsFormsAppCN.exe "..\\Config\\host2.xml"
-timeout 1
-start .\WindowsFormsAppCN.exe "..\\Config\\host3.xml"
